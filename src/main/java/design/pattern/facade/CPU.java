21106b9e2c49f2ea7ad4a1a7c00a867ae7458912
cherbini <cherbini@qq.com>
2018-11-05 10:22:15 +0800

    新增外观模式 外观模式,通过简化子系统接口,使客户端和子系统解耦 降低子系统调用的复杂度 在外观类完成一系列子系统的调度(增加的业务逻辑) 2018年11月5日

---
+++ src/main/java/design/pattern/facade/CPU.java
@@ -0,0 +1,18 @@
+package design.pattern.facade;
+
+/**
+ * @author cherbini
+ * 2018/11/5 10:13
+ */
+public class CPU
+{
+    public void startUp()
+    {
+        System.out.println("cpu is startup!");
+    }
+
+    public void shutdown()
+    {
+        System.out.println("cpu is shutdown!");
+    }
+}
---
+++ src/main/java/design/pattern/facade/Memory.java
@@ -0,0 +1,18 @@
+package design.pattern.facade;
+
+/**
+ * @author cherbini
+ * 2018/11/5 10:15
+ */
+public class Memory
+{
+    public void startUp()
+    {
+        System.out.println("memory is startup!");
+    }
+
+    public void shutdown()
+    {
+        System.out.println("memory is shutdown!");
+    }
+}
