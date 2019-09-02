c25b07fe33834fd77037ac582b9bfcdb1c9954d9
cherbini <cherbini@qq.com>
2019-09-02 16:42:32 +0800

    push

--- src/main/java/algorithm/LeetCode.java
+++ src/main/java/algorithm/LeetCode.java
@@ -42,6 +42,127 @@ public class LeetCode
     }
 
 
+    public boolean detectCapitalUse(String word)
+    {
+
+        int len = word.length();
+        int count = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if (Character.isUpperCase(word.charAt(i)))
+            {
+                count++;
+            }
+        }
+        if (count == len)
+        {
+            return true;
+        }
+        if (count == 0)
+        {
+            return true;
+        }
+        return Character.isUpperCase(word.charAt(0)) && count == 1;
+    }
+
+    public int[] findDiagonalOrder(int[][] matrix)
+    {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] arr = new int[row * col];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i != j)
+                {
+
+                }
+
+            }
+        }
+        return null;
+
+    }
+
+
+    public class TreeNode
+    {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root)
+    {
+        if (Objects.isNull(root))
+        {
+            return null;
+        }
+        return resucive(root);
+
+
+    }
+
+    private TreeNode resucive(TreeNode root)
+    {
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        root.left = right;
+        root.right = left;
+        if (right != null)
+        {
+            resucive(right);
+        }
+        if (left != null)
+        {
+            resucive(left);
+        }
+        return root;
+
+    }
+
+    public int[][] matrixReshape(int[][] nums, int r, int c)
+    {
+        int[][] result = new int[r][c];
+
+        List<Integer> linkedList = new LinkedList<>();
+
+        for (int x = 0; x < nums.length; x++)
+        {
+            for (int z = 0; z< nums[0].length; z++)
+            {
+                linkedList.add(nums[x][z]);
+            }
+        }
+        Iterator<Integer> iterator = linkedList.iterator();
+        for (int i = 0; i < r; i++)
+        {
+            if (c==0){
+
+            }
+            for (int j = 0; j < c; j++)
+            {
+                if (iterator.hasNext())
+                {
+                    result[i][j] = iterator.next();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args)
     {
         System.out.println(lengthOfLongestSubstring("pwwkew"));
